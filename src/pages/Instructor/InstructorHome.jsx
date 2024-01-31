da4f5d4db8e6c39c92cfbcb68124b560fb818fa7
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-01-31 21:23:08 +0530

    Fontsizes fixed and order of instructor dashboard cards switched

--- src/pages/Instructor/InstructorHome.jsx
+++ src/pages/Instructor/InstructorHome.jsx
@@ -1,16 +1,15 @@
-import { Box, Button, HStack, Heading, Image, Stack, Text, VStack } from '@chakra-ui/react'
+import { Button, HStack, Heading, Image, Stack, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
-import { Link } from 'react-router-dom'
-import MainWrapper from '../../components/MainWrapper'
-import TransitionWrapper from '../../components/Transition'
-import { MdBook } from 'react-icons/md'
-import { RiBookOpenFill, RiVideoAddFill } from 'react-icons/ri'
 import { BiStats } from 'react-icons/bi'
 import { FaMoneyBill } from 'react-icons/fa'
+import { RiBookOpenFill, RiVideoAddFill } from 'react-icons/ri'
+import { Link } from 'react-router-dom'
 import mycourses from "../../assets/images/1_mycourses.png"
 import newcourse from "../../assets/images/2_newcourse.png"
 import stats from "../../assets/images/3_stats.png"
-import earnings from "../../assets/images/4_earning.png"
+import earnings from "../../assets/images/4_earnings.png"
+import MainWrapper from '../../components/MainWrapper'
+import TransitionWrapper from '../../components/Transition'
 
 const InstructorHome = () => {
     return (
@@ -18,17 +17,17 @@ const InstructorHome = () => {
             <TransitionWrapper>
                 <MainWrapper pt={20} pb={12}>
                     <VStack gap={0}>
-                        <Heading mt={['6', '6', '8', '8']} textAlign={'center'} fontFamily={'Young Serif'}>Instructor's Dashboard</Heading>
-                        <Text mt={['1', '1', '2', '2']} width={['80%', '', '', '']} textAlign={'center'} >Hey Deependra👋, manage your courses, statistics, earnings and more.</Text>
+                        <Heading mt={['6', '6', '8', '8']} textAlign={'center'} fontFamily={'Young Serif'} fontSize={['2xl','2xl','3xl','4xl']}>Instructor's Dashboard</Heading>
+                        <Text mt={['1', '1', '2', '2']} fontSize={['sm','sm','md','md']} width={['80%', '', '', '']} textAlign={'center'} >Hey Deependra👋, manage your courses, statistics, earnings and more.</Text>
                         <Stack mt={'2rem'} flexWrap={'wrap'} gap={'8'} direction={['column', 'column', 'row', 'row']} alignItems={['center', 'center', 'center', 'center']} justifyContent={['flex-start', 'flex-start', 'center', 'center']}>
 
                             <InstructorCard title={"My Courses"} description={"Have a look at all the courses you have created in this menu."} icon={<RiBookOpenFill />} buttonText={`See Courses`} route={"/instructor/courses"} image_url={mycourses} />
 
-                            <InstructorCard title={"Create a Course"} description={"Create a new course and start teaching students."} icon={<RiVideoAddFill />} buttonText={`Create Course`} route={"/instructor/courses/new"} image_url={newcourse} />
+                            <InstructorCard title={"My Stats"} description={"Look into the insights, views, new learners and more."} icon={<BiStats />} buttonText={`My Stats`} route={"/instructor/stats"} image_url={stats} />
 
-                            <InstructorCard title={"My Stats"} description={"Create a new course and start teaching students."} icon={<BiStats />} buttonText={`My Stats`} route={"/instructor/stats"} image_url={stats} />
+                            <InstructorCard title={"New Course"} description={"Create a new course and start teaching students."} icon={<RiVideoAddFill />} buttonText={`Create Course`} route={"/instructor/courses/new"} image_url={newcourse} />
 
-                            <InstructorCard title={"My Earning"} description={"Create a new course and start teaching students."} icon={<FaMoneyBill />} buttonText={`My Earning`} route={"/instructor/earning"} image_url={earnings} />
+                            <InstructorCard title={"My Earning"} description={"Introspect your earning curve in my earning section."} icon={<FaMoneyBill />} buttonText={`My Earning`} route={"/instructor/earning"} image_url={earnings} />
 
 
                         </Stack>
