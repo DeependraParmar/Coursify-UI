99c5d7e76601c0efe82f7746205bf135127da7c2
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-03-11 10:01:51 +0530

    Teach on coursify hidden from non-authenticated view

--- src/pages/Layout/Header.jsx
+++ src/pages/Layout/Header.jsx
@@ -241,19 +241,18 @@ const NavProfile = React.memo(({ isAuthenticated, isVerifiedInstructor, user }) 
                   <MenuDivider />
                   <MenuItem fontSize={'sm'} gap={'2'} onClick={onDrawerClose} _hover={{ bg: "#e2f2ff" }}><MdOutlinePassword /><Link className='width-full' to={'/forgot-password'}>Forgot Password</Link></MenuItem>
                   <MenuItem fontSize={'sm'} gap={'2'} onClick={onDrawerClose} _hover={{ bg: "#e2f2ff" }}><MdOutlineLockReset /><Link className='width-full' to={'/reset-password'}>Reset Password</Link></MenuItem>
-                  <MenuDivider />
+                  {
+                    isVerifiedInstructor ?
+                      <MenuItem fontSize={'sm'} onClick={onDrawerClose} _hover={{ bg: "#e2f2ff" }} gap={'2'}><FaChalkboardTeacher /><Link className='width-full' to={'/instructor/dashboard'}>Instructor View</Link></MenuItem>
+                      :
+                      <MenuItem fontSize={'sm'} onClick={onDrawerClose} _hover={{ bg: "#e2f2ff" }} gap={'2'}><FaChalkboardTeacher /><Link className='width-full' to={'/instructor/register'}>Teach on Coursify</Link></MenuItem>
+                  }
                 </MenuGroup>
               ) : (
                 <MenuDivider />
               )
             }
             <MenuGroup>
-              {
-                isVerifiedInstructor ?
-                  <MenuItem fontSize={'sm'} onClick={onDrawerClose} _hover={{ bg: "#e2f2ff" }} gap={'2'}><FaChalkboardTeacher /><Link className='width-full' to={'/instructor/dashboard'}>Instructor View</Link></MenuItem>
-                  :
-                  <MenuItem fontSize={'sm'} onClick={onDrawerClose} _hover={{ bg: "#e2f2ff" }} gap={'2'}><FaChalkboardTeacher /><Link className='width-full' to={'/instructor/register'}>Teach on Coursify</Link></MenuItem>
-              }
               <MenuItem fontSize={'sm'} onClick={onDrawerClose} _hover={{ bg: "#e2f2ff" }} gap={'2'}><AiOutlineQuestionCircle /><Link className='width-full' to={'/faq'}>FAQ</Link></MenuItem>
             </MenuGroup>
             <MenuDivider />
@@ -265,7 +264,7 @@ const NavProfile = React.memo(({ isAuthenticated, isVerifiedInstructor, user }) 
               ) : (
                 <>
                   <Button onClick={onDrawerClose} variant={'solid'} colorScheme={'purple'} color={'white'} _hover={{ bg: '#240055' }} fontSize={['xs', 'xs', 'sm', 'sm']} mr={2} size={['sm', 'sm', 'md', 'md']} gap={'2'}><BiLogIn /><Link to={'/login'} >Login</Link></Button>
-                  <Button onClick={onDrawerClose} variant={'solid'} fontSize={['xs', 'xs', 'sm', 'sm']} size={['sm', 'sm', 'md', 'md']} gap={'2'}><BiPlus /><Link to={'/signup'} >SignUp</Link></Button>
+                  <Button onClick={onDrawerClose} variant={'solid'} fontSize={['xs', 'xs', 'sm', 'sm']} size={['sm', 'sm', 'md', 'md']} gap={'2'}><BiPlus /><Link to={'/register'} >SignUp</Link></Button>
                 </>
               )
             }
