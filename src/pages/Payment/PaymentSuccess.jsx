d0c11a3259687ed83df983d43c32e13bcbd6e78a
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-03-22 08:05:21 +0530

    Added the animated success icon in payment success

--- src/pages/Payment/PaymentSuccess.jsx
+++ src/pages/Payment/PaymentSuccess.jsx
@@ -1,7 +1,6 @@
 import { Button, Heading, Image, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 import { Link, useSearchParams } from 'react-router-dom'
-import success from '../../assets/images/paymentsuccess.avif'
 import MainWrapper from '../../components/MainWrapper'
 import TransitionWrapper from '../../components/Transition'
 
@@ -11,8 +10,8 @@ const PaymentSuccess = () => {
     return (
         <TransitionWrapper>
             <MainWrapper pt={24} pb={12}>
-                <VStack width={['95%', '95%', '30%', '30%']} margin={'auto'}>
-                    <Image src={success} />
+                <VStack width={['95%', '95%', '30%', '30%']} margin={'auto'} textAlign={'center'}>
+                    <Image width={['60%', '60%', '60%', '60%']} position={'relative'} left={12} src={'https://res.cloudinary.com/dmmrtqe8q/image/upload/v1710931760/confetti_cnpgkf.gif'} />
                     <Heading pb={2} fontSize={['2xl','2xl','4xl','4xl']}>Payment Successfull</Heading>
                     <Text fontSize={'sm'}>Your purchase's reference id is: <b>{reference}</b></Text>
                     <Button size={['sm', 'sm', 'md','md']}><Link to={'/'}>Back to Home</Link></Button>
