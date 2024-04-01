600bc607074a93c914417925fbcc2bead07292df
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-04-01 07:12:56 +0530

    Done with createCourseReducer

--- src/redux/reducers/instructorReducer.js
+++ src/redux/reducers/instructorReducer.js
@@ -1,5 +1,24 @@
 import { createReducer } from "@reduxjs/toolkit";
 
 export const instructorReducer = createReducer({}, {
-    
+    createCourseRequest: (state) => {
+        state.loading = true;
+    },
+    createCourseSuccess: (state, action) => {
+        state.loading = false;
+        state.message = action.payload;
+    },
+    createCourseFail: (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+    },
+
+
+
+    clearError: (state) => {
+        state.error = null;
+    },
+    clearMessage: (state) => {
+        state.message = null;
+    }
 });
