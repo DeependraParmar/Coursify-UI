f00a3e8d2d8063e0c0357100ae0c15bde418ea75
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-04-23 05:49:46 +0530

    Resolved upload button's abnormal style

--- src/pages/Instructor/InstructorRegistration.jsx
+++ src/pages/Instructor/InstructorRegistration.jsx
@@ -1,15 +1,16 @@
-import { Box, Button, Heading, Input, InputGroup, InputLeftElement, Stack, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, Heading, Input, InputGroup, InputLeftElement, Stack, Tab, TabList, TabPanel, TabPanels, Tabs, Text, VStack } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
-import { MdPhone, MdVerified } from 'react-icons/md'
+import { MdPhone, MdPreview, MdVerified } from 'react-icons/md'
 import ReactQuill from 'react-quill'
 import { useDispatch, useSelector } from 'react-redux'
-import { fileUploadCSS } from '../../../controllers'
+import { fileUploadCSS, sanitizedHTML } from '../../../controllers'
 import LoadingComponent from '../../components/Loading'
 import MainWrapper from '../../components/MainWrapper'
 import TransitionWrapper from "../../components/Transition"
 import { registerAsInstructor } from '../../redux/actions/user'
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom'
+import { FaEdit } from 'react-icons/fa'
 
 const InstructorRegistration = () => {
     const [phone, setPhone] = useState('');
@@ -37,14 +38,14 @@ const InstructorRegistration = () => {
     }
 
     useEffect(() => {
-        if(message){
+        if (message) {
             toast.success(message);
-            dispatch( {type: 'clearMessage' });
+            dispatch({ type: 'clearMessage' });
             navigate('/');
         }
-        if(error){
+        if (error) {
             toast.error(error);
-            dispatch({type: 'clearError' });
+            dispatch({ type: 'clearError' });
         }
     }, [dispatch, error, message]);
 
@@ -88,7 +89,7 @@ const InstructorRegistration = () => {
                         Instructor Signup
                     </Heading>
                     <Text fontSize={'sm'} textAlign={'center'}>Note: Your <b>Name</b> and <b>Email</b> associated with your profile will be considered by default.</Text>
-                    <form style={{width: '100%'}} onSubmit={(event) => formSubmitHandler(event)}>
+                    <form style={{ width: '100%' }} onSubmit={(event) => formSubmitHandler(event)}>
                         <Stack spacing={4}>
                             <InputGroup>
                                 <InputLeftElement pointerEvents="none">
@@ -100,59 +101,100 @@ const InstructorRegistration = () => {
                                     required={true}
                                     type="number"
                                     placeholder="Phone Number"
-                                    focusBorderColor={ phone && phone.toString().length === 10 ? '#5000bb' : 'red.500' }
+                                    focusBorderColor={phone && phone.toString().length === 10 ? '#5000bb' : 'red.500'}
                                     fontSize={'sm'}
                                 />
                             </InputGroup>
 
-                            <Box width={'full'} border={'1px solid #e2e8f0'} height={'150px'} borderRadius={'8px'}>
-                                <ReactQuill
-                                    placeholder='Mention your Work Experience here'
-                                    value={experience}
-                                    onChange={setExperience}
-                                    modules={modules}
-                                    formats={formats}
-                                    bounds={'#root'}
-                                    theme="snow"
-                                    className='quill'
-                                    style={{ height: '70%' }}
-                                />
-                            </Box>
 
-                            <Box width={'full'} border={'1px solid #e2e8f0'} height={'150px'} borderRadius={'8px'}>
-                                <ReactQuill
-                                    placeholder='Mention your Educational Background here'
-                                    value={education}
-                                    onChange={setEducation}
-                                    modules={modules}
-                                    formats={formats}
-                                    bounds={'#root'}
-                                    theme="snow"
-                                    className='quill'
-                                    style={{ height: '70%' }}
-                                />
-                            </Box>
 
-                            <Box width={'full'} border={'1px solid #e2e8f0'} height={'150px'} borderRadius={'8px'}>
-                                <ReactQuill
-                                    placeholder='Mention your skills here'
-                                    value={skills}
-                                    onChange={setSkills}
-                                    modules={modules}
-                                    formats={formats}
-                                    bounds={'#root'}
-                                    theme="snow"
-                                    className='quill'
-                                    style={{ height: '70%' }}
-                                />
-                            </Box>
+                            <Tabs className='dropboxTab dropboxTab-height grayScrollbar' isFitted width={'full'} variant='enclosed-colored' colorScheme='purple'>
+                                <TabList width={'full'}>
+                                    <Tab fontSize={'sm'} gap={2}>Editor <FaEdit /> </Tab>
+                                    <Tab fontSize={'sm'} gap={2}>Preview <MdPreview /> </Tab>
+                                </TabList>
+                                <TabPanels>
+                                    <TabPanel>
+                                        <Box width={'full'} border={'1px solid #e2e8f0'} height={'150px'} borderRadius={'8px'}>
+                                            <ReactQuill
+                                                placeholder='Mention your Work Experience here'
+                                                value={experience}
+                                                onChange={setExperience}
+                                                modules={modules}
+                                                formats={formats}
+                                                bounds={'#root'}
+                                                theme="snow"
+                                                className='quill'
+                                                style={{ height: '70%' }}
+                                            />
+                                        </Box>
+                                    </TabPanel>
+                                    <TabPanel>
+                                        <Text py={4} px={8} fontSize={'sm'} dangerouslySetInnerHTML={{ __html: sanitizedHTML(experience) }} ></Text>
+                                    </TabPanel>
+                                </TabPanels>
+                            </Tabs>
 
-                                <Input className='resumeUploadButton' onChange={changeImageHandler} type={'file'} accept='application/pdf' css={{ "&::file-selector-button": fileUploadCSS}} />
+                            <Tabs className='dropboxTab dropboxTab-height grayScrollbar' isFitted width={'full'} variant='enclosed-colored' colorScheme='purple'>
+                                <TabList width={'full'}>
+                                    <Tab fontSize={'sm'} gap={2}>Editor <FaEdit /> </Tab>
+                                    <Tab fontSize={'sm'} gap={2}>Preview <MdPreview /> </Tab>
+                                </TabList>
+                                <TabPanels>
+                                    <TabPanel>
+                                        <Box width={'full'} border={'1px solid #e2e8f0'} height={'150px'} borderRadius={'8px'}>
+                                            <ReactQuill
+                                                placeholder='Mention your Educational Background here'
+                                                value={education}
+                                                onChange={setEducation}
+                                                modules={modules}
+                                                formats={formats}
+                                                bounds={'#root'}
+                                                theme="snow"
+                                                className='quill'
+                                                style={{ height: '70%' }}
+                                            />
+                                        </Box>
+                                    </TabPanel>
+                                    <TabPanel>
+                                        <Text py={4} px={8} fontSize={'sm'} dangerouslySetInnerHTML={{ __html: sanitizedHTML(education) }} ></Text>
+                                    </TabPanel>
+                                </TabPanels>
+                            </Tabs>
+
+                            <Tabs className='dropboxTab dropboxTab-height grayScrollbar' isFitted width={'full'} variant='enclosed-colored' colorScheme='purple'>
+                                <TabList width={'full'}>
+                                    <Tab fontSize={'sm'} gap={2}>Editor <FaEdit /> </Tab>
+                                    <Tab fontSize={'sm'} gap={2}>Preview <MdPreview /> </Tab>
+                                </TabList>
+                                <TabPanels>
+                                    <TabPanel>
+                                        <Box width={'full'} border={'1px solid #e2e8f0'} height={'150px'} borderRadius={'8px'}>
+                                            <ReactQuill
+                                                placeholder='Mention your skills here'
+                                                value={skills}
+                                                onChange={setSkills}
+                                                modules={modules}
+                                                formats={formats}
+                                                bounds={'#root'}
+                                                theme="snow"
+                                                className='quill'
+                                                style={{ height: '70%' }}
+                                            />
+                                        </Box>
+                                    </TabPanel>
+                                    <TabPanel>
+                                        <Text py={4} px={8} fontSize={'sm'} dangerouslySetInnerHTML={{ __html: sanitizedHTML(skills) }} ></Text>
+                                    </TabPanel>
+                                </TabPanels>
+                            </Tabs>
+
+                            <Input className='resumeUploadButton' onChange={changeImageHandler} type={'file'} accept='application/pdf' css={{ "&::file-selector-button": fileUploadCSS }} />
                             {
                                 pdfPrev && <iframe src={pdfPrev} alt="" width={'100%'} height={'770'} />
                             }
-                            
-                            <Button type='submit' isDisabled={!phone || !experience || !education || !skills || !pdf || phone.toString().length !== 10 } colorScheme='purple' gap={2}>Submit <MdVerified /></Button>
+
+                            <Button type='submit' isDisabled={!phone || !experience || !education || !skills || !pdf || phone.toString().length !== 10} colorScheme='purple' gap={2}>Submit <MdVerified /></Button>
                         </Stack>
                     </form>
 
