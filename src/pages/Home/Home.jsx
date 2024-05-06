ae04a726efc6081e579716c5fc14e205fd572154
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-05-06 11:38:24 +0530

    Featured courses replaced by real courses

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,6 +9,9 @@ import CourseCard from '../../components/CourseCard.jsx';
 import GridCourseWrapper from '../../components/GridCourseWrapper.jsx';
 import MainWrapper from '../../components/MainWrapper.jsx';
 import TransitionWrapper from '../../components/Transition.jsx';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import { getAllCourses } from '../../redux/actions/course.js';
 
 const Home = () => {
 
@@ -44,18 +47,36 @@ const CarouselComponent = () => {
 
 
 export const FeaturedCoursesComponent = () => {
+  const { loading, error, message, courses } = useSelector(state => state.course);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllCourses());
+  }, []);
+
+  useEffect(() => {
+    if(error){
+      toast.error(error);
+      dispatch({ type: 'clearError' });
+    }
+    if(message){
+      toast.success(message);
+      dispatch({ type: 'clearMessage' });
+    }
+  }, [dispatch, error, message]);
+
   return <MainWrapper pt={'4'} pb={'10'}>
     {/* Home Page Features */}
     <VStack spacing={'12'}>
       <Heading id='featured_section' size="xl" fontFamily={"Young Serif"} textAlign="center">Featured Courses</Heading>
       <GridCourseWrapper>
-        {courses.map((course, index) => (
+        {courses && courses.map((course, index) => (
           <GridItem width={'full'} key={index}>
-            <Link to={`/courses/${course.id}`} >
+            <Link to={`/courses/${course._id}`} >
               <CourseCard
-                image_url={course.image_url}
+                image_url={course.poster.url}
                 course_title={course.title}
-                course_description={course.course_description}
+                course_description={course.description}
                 created_by={course.created_by}
                 price={course.price}
               />
