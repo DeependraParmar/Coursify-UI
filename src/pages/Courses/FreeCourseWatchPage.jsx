a0474640872e2c691c071d40cd29890251e76fce
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-05-08 09:17:43 +0530

    Added the tab panel to the free course watch page

--- src/pages/Courses/FreeCourseWatchPage.jsx
+++ src/pages/Courses/FreeCourseWatchPage.jsx
@@ -1,4 +1,4 @@
-import { AspectRatio, Box, Button, Divider, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, HStack, Image, Menu, MenuDivider, MenuGroup, MenuItem, Stack, Text, VStack, useDisclosure, } from '@chakra-ui/react'
+import { AspectRatio, Box, Button, Divider, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, HStack, Image, Menu, MenuDivider, MenuGroup, MenuItem, Stack, Tab, TabList, TabPanel, TabPanels, Tabs, Text, VStack, useDisclosure, } from '@chakra-ui/react'
 import React, { useEffect } from 'react'
 import { AiFillLeftCircle } from 'react-icons/ai'
 import { useDispatch, useSelector } from 'react-redux'
@@ -102,7 +102,22 @@ const CourseWatchPage = () => {
                                             }
                                         </AspectRatio>
                                         <Text pt={'4'} fontFamily={'Young Serif'} fontSize={['2xl', '2xl', '2xl', '4xl']} lineHeight={'shorter'}>{lecture?.title || course?.title}</Text>
-                                        <Text py={'1'} fontSize={'sm'} dangerouslySetInnerHTML={{ __html: sanitizedHTML(lecture?.description || course?.description) }} ></Text>
+                                        <Tabs mt={'4'} colorScheme='purple'>
+                                            <TabList>
+                                                <Tab fontSize={'sm'}>Content</Tab>
+                                                <Tab fontSize={'sm'}>Comment</Tab>
+                                                <Tab fontSize={'sm'}>Doubts</Tab>
+                                                <Tab fontSize={'sm'}>Downloads</Tab>
+                                            </TabList>
+                                            <TabPanels>
+                                                <TabPanel>
+                                                    <Text py={'1'} fontSize={'sm'} dangerouslySetInnerHTML={{ __html: sanitizedHTML(lecture?.description || course?.description) }} ></Text>
+                                                </TabPanel>
+                                                <TabPanel></TabPanel>
+                                                <TabPanel></TabPanel>
+                                                <TabPanel></TabPanel>
+                                            </TabPanels>
+                                        </Tabs>
                                     </Box>
 
                                     <VStack h={['', '', '400px', '530px']} className='grayScrollbar' display={['none', 'none', 'block', 'block']} p={'2'} width={['90%', '90%', '30%', '30%']} overflowY={'auto'} border={'1px solid rgb(0,0,0,0.1)'}  >
